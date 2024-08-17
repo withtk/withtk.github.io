940ab46b5830c788b877ebfdea038857c4a53dc8
mcPro <dnf126@gmail.com>
2024-08-18 03:51:54 +0900

    Add | toast ui Editor

---
+++ toast-ui-editor/test/script1.js
@@ -0,0 +1,15 @@
+
+const a = ()=>{
+  // alert('aaaa');
+  document.getElementById('ppp').innerHTML = 'aaaa';
+
+}
+const b = ()=>{
+  // alert('aaaa');
+  document.getElementById('ppp').innerHTML = 'bbbb';
+
+}
+const onAA = ()=>{
+  a();
+}
+ 
---
+++ toast-ui-editor/test/script2.js
@@ -0,0 +1,9 @@
+
+const a = ()=>{
+  document.getElementById('ppp').innerHTML = 'script2 aaaa';
+
+  
+}
+const onAA = ()=>{
+  a();
+}
