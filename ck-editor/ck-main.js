b9b724e1bbd2b7141f14c1426b13fea4f8070976
mcPro <dnf126@gmail.com>
2024-08-20 16:59:04 +0900

    ck-editor | test1

--- ck-editor/ck-main.js
+++ ck-editor/ck-main.js
@@ -1,151 +1,151 @@
-// import {
-//   ClassicEditor,
-//   AccessibilityHelp,
-//   Alignment,
-//   AutoLink,
-//   Autosave,
-//   BalloonToolbar,
-//   BlockQuote,
-//   BlockToolbar,
-//   CodeBlock,
-//   Essentials,
-//   Heading,
-//   HorizontalLine,
-//   Indent,
-//   IndentBlock,
-//   Link,
-//   Paragraph,
-//   SelectAll,
-//   SpecialCharacters,
-//   Table,
-//   TableCaption,
-//   TableCellProperties,
-//   TableColumnResize,
-//   TableProperties,
-//   TableToolbar,
-//   Undo
-// } from './ckeditor5/ckeditor5';
-//
-// const editorConfig = {
-//   toolbar: {
-//     items: [
-//       'undo',
-//       'redo',
-//       '|',
-//       'selectAll',
-//       '|',
-//       'heading',
-//       '|',
-//       'specialCharacters',
-//       'horizontalLine',
-//       'link',
-//       'insertTable',
-//       'blockQuote',
-//       'codeBlock',
-//       '|',
-//       'alignment',
-//       '|',
-//       'outdent',
-//       'indent',
-//       '|',
-//       'accessibilityHelp'
-//     ],
-//     shouldNotGroupWhenFull: false
-//   },
-//   plugins: [
-//     AccessibilityHelp,
-//     Alignment,
-//     AutoLink,
-//     Autosave,
-//     BalloonToolbar,
-//     BlockQuote,
-//     BlockToolbar,
-//     CodeBlock,
-//     Essentials,
-//     Heading,
-//     HorizontalLine,
-//     Indent,
-//     IndentBlock,
-//     Link,
-//     Paragraph,
-//     SelectAll,
-//     SpecialCharacters,
-//     Table,
-//     TableCaption,
-//     TableCellProperties,
-//     TableColumnResize,
-//     TableProperties,
-//     TableToolbar,
-//     Undo
-//   ],
-//   balloonToolbar: ['link'],
-//   blockToolbar: ['link', 'insertTable', '|', 'outdent', 'indent'],
-//   heading: {
-//     options: [
-//       {
-//         model: 'paragraph',
-//         title: 'Paragraph',
-//         class: 'ck-heading_paragraph'
-//       },
-//       {
-//         model: 'heading1',
-//         view: 'h1',
-//         title: 'Heading 1',
-//         class: 'ck-heading_heading1'
-//       },
-//       {
-//         model: 'heading2',
-//         view: 'h2',
-//         title: 'Heading 2',
-//         class: 'ck-heading_heading2'
-//       },
-//       {
-//         model: 'heading3',
-//         view: 'h3',
-//         title: 'Heading 3',
-//         class: 'ck-heading_heading3'
-//       },
-//       {
-//         model: 'heading4',
-//         view: 'h4',
-//         title: 'Heading 4',
-//         class: 'ck-heading_heading4'
-//       },
-//       {
-//         model: 'heading5',
-//         view: 'h5',
-//         title: 'Heading 5',
-//         class: 'ck-heading_heading5'
-//       },
-//       {
-//         model: 'heading6',
-//         view: 'h6',
-//         title: 'Heading 6',
-//         class: 'ck-heading_heading6'
-//       }
-//     ]
-//   },
-//   initialData:
-//     '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n    You\'ve successfully created a CKEditor 5 project. This powerful text editor will enhance your application, enabling rich text editing\n    capabilities that are customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n    <li>\n        <strong>Integrate into your app</strong>: time to bring the editing into your application. Take the code you created and add to your\n        application.\n    </li>\n    <li>\n        <strong>Explore features:</strong> Experiment with different plugins and toolbar options to discover what works best for your needs.\n    </li>\n    <li>\n        <strong>Customize your editor:</strong> Tailor the editor\'s configuration to match your application\'s style and requirements. Or even\n        write your plugin!\n    </li>\n</ol>\n<p>\n    Keep experimenting, and don\'t hesitate to push the boundaries of what you can achieve with CKEditor 5. Your feedback is invaluable to us\n    as we strive to improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n    <li>📝 <a href="https://orders.ckeditor.com/trial/premium-features">Trial sign up</a>,</li>\n    <li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n    <li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n    <li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n    <li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n    See this text, but the editor is not starting up? Check the browser\'s console for clues and guidance. It may be related to an incorrect\n    license key if you use premium features or another feature-related requirement. If you cannot make it work, file a GitHub issue, and we\n    will help as soon as possible!\n</p>\n',
-//   link: {
-//     addTargetToExternalLinks: true,
-//     defaultProtocol: 'https://',
-//     decorators: {
-//       toggleDownloadable: {
-//         mode: 'manual',
-//         label: 'Downloadable',
-//         attributes: {
-//           download: 'file'
-//         }
-//       }
-//     }
-//   },
-//   placeholder: 'Type or paste your content here!',
-//   table: {
-//     contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
-//   }
-// };
-//
+import {
+  ClassicEditor,
+  AccessibilityHelp,
+  Alignment,
+  AutoLink,
+  Autosave,
+  BalloonToolbar,
+  BlockQuote,
+  BlockToolbar,
+  CodeBlock,
+  Essentials,
+  Heading,
+  HorizontalLine,
+  Indent,
+  IndentBlock,
+  Link,
+  Paragraph,
+  SelectAll,
+  SpecialCharacters,
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+  Undo
+} from './ckeditor5/ckeditor5';
+
+const editorConfig = {
+  toolbar: {
+    items: [
+      'undo',
+      'redo',
+      '|',
+      'selectAll',
+      '|',
+      'heading',
+      '|',
+      'specialCharacters',
+      'horizontalLine',
+      'link',
+      'insertTable',
+      'blockQuote',
+      'codeBlock',
+      '|',
+      'alignment',
+      '|',
+      'outdent',
+      'indent',
+      '|',
+      'accessibilityHelp'
+    ],
+    shouldNotGroupWhenFull: false
+  },
+  plugins: [
+    AccessibilityHelp,
+    Alignment,
+    AutoLink,
+    Autosave,
+    BalloonToolbar,
+    BlockQuote,
+    BlockToolbar,
+    CodeBlock,
+    Essentials,
+    Heading,
+    HorizontalLine,
+    Indent,
+    IndentBlock,
+    Link,
+    Paragraph,
+    SelectAll,
+    SpecialCharacters,
+    Table,
+    TableCaption,
+    TableCellProperties,
+    TableColumnResize,
+    TableProperties,
+    TableToolbar,
+    Undo
+  ],
+  balloonToolbar: ['link'],
+  blockToolbar: ['link', 'insertTable', '|', 'outdent', 'indent'],
+  heading: {
+    options: [
+      {
+        model: 'paragraph',
+        title: 'Paragraph',
+        class: 'ck-heading_paragraph'
+      },
+      {
+        model: 'heading1',
+        view: 'h1',
+        title: 'Heading 1',
+        class: 'ck-heading_heading1'
+      },
+      {
+        model: 'heading2',
+        view: 'h2',
+        title: 'Heading 2',
+        class: 'ck-heading_heading2'
+      },
+      {
+        model: 'heading3',
+        view: 'h3',
+        title: 'Heading 3',
+        class: 'ck-heading_heading3'
+      },
+      {
+        model: 'heading4',
+        view: 'h4',
+        title: 'Heading 4',
+        class: 'ck-heading_heading4'
+      },
+      {
+        model: 'heading5',
+        view: 'h5',
+        title: 'Heading 5',
+        class: 'ck-heading_heading5'
+      },
+      {
+        model: 'heading6',
+        view: 'h6',
+        title: 'Heading 6',
+        class: 'ck-heading_heading6'
+      }
+    ]
+  },
+  initialData:
+    '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n    You\'ve successfully created a CKEditor 5 project. This powerful text editor will enhance your application, enabling rich text editing\n    capabilities that are customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n    <li>\n        <strong>Integrate into your app</strong>: time to bring the editing into your application. Take the code you created and add to your\n        application.\n    </li>\n    <li>\n        <strong>Explore features:</strong> Experiment with different plugins and toolbar options to discover what works best for your needs.\n    </li>\n    <li>\n        <strong>Customize your editor:</strong> Tailor the editor\'s configuration to match your application\'s style and requirements. Or even\n        write your plugin!\n    </li>\n</ol>\n<p>\n    Keep experimenting, and don\'t hesitate to push the boundaries of what you can achieve with CKEditor 5. Your feedback is invaluable to us\n    as we strive to improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n    <li>📝 <a href="https://orders.ckeditor.com/trial/premium-features">Trial sign up</a>,</li>\n    <li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n    <li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n    <li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n    <li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n    See this text, but the editor is not starting up? Check the browser\'s console for clues and guidance. It may be related to an incorrect\n    license key if you use premium features or another feature-related requirement. If you cannot make it work, file a GitHub issue, and we\n    will help as soon as possible!\n</p>\n',
+  link: {
+    addTargetToExternalLinks: true,
+    defaultProtocol: 'https://',
+    decorators: {
+      toggleDownloadable: {
+        mode: 'manual',
+        label: 'Downloadable',
+        attributes: {
+          download: 'file'
+        }
+      }
+    }
+  },
+  placeholder: 'Type or paste your content here!',
+  table: {
+    contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+  }
+};
+
 // ClassicEditor.create(document.querySelector('#editor'), editorConfig).then((editor) => {
 //     window.editor = editor;
 //     // editor.setData( '<p>Modified from the console! initilized data</p>' );
@@ -157,12 +157,9 @@
 
 
 
-import { ClassicEditor, Essentials, Paragraph, Bold, Italic, Font } from "./ckeditor5/ckeditor5";
+// import { ClassicEditor, Essentials, Paragraph, Bold, Italic, Font } from "./ckeditor5/ckeditor5";
 
-ClassicEditor.create(document.querySelector("#editor"), {
-  plugins: [Essentials, Paragraph, Bold, Italic, Font],
-  toolbar: ["undo", "redo", "|", "bold", "italic", "|", "fontSize", "fontFamily", "fontColor", "fontBackgroundColor"],
-})
+ClassicEditor.create(document.querySelector("#editor"), editorConfig)
   .then((editor) => {
     window.editor = editor;
     // editor.setData( '<p>Modified from the console! initilized data</p>' );
