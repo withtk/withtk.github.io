0df032bc41bb120ce6ed7c4541fcd08cb3a35104
mcPro <dnf126@gmail.com>
2025-02-15 08:51:00 +0900

    blog | add FB key

---
+++ blog/assets/Redirect-BJL1Isra.js
@@ -0,0 +1,1 @@
+import{d as e}from "./index-Cc1mhAAw.js";const a=e({__name:"Redirect",setup(t){return window.location.href="http://www.naver.com/",(n, o)=>null}});export{a as default};
--- blog/assets/Redirect-BOUefPao.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from "./index-BoJ1F8L8.js";const a=e({__name:"Redirect",setup(t){return window.location.href="http://www.naver.com/",(n, o)=>null}});export{a as default};
