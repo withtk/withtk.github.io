4c6857d1a9ea66d8ccd8df5eb481e2ffd32183b0
mcPro <dnf126@gmail.com>
2025-06-09 08:41:08 +0900

    wang | seperated home.html 22

--- wangcode/src/page/Home.jsx
+++ wangcode/src/page/Home.jsx
@@ -1,159 +1,81 @@
-import React from 'react';
-import '../styles/Home.css';
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import '../styles/Home.css'
+import { useTheme } from '../context/ThemeContext'
 
-const Home = () => {
-  const features = [
-    {
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
-          <path d="M244.8,150.4a8,8,0,0,1-11.2-1.6A51.6,51.6,0,0,0,192,128a8,8,0,0,1-7.37-4.89,8,8,0,0,1,0-6.22A8,8,0,0,1,192,112a24,24,0,1,0-23.24-30,8,8,0,1,1-15.5-4A40,40,0,1,1,219,117.51a67.94,67.94,0,0,1,27.43,21.68A8,8,0,0,1,244.8,150.4ZM190.92,212a8,8,0,1,1-13.84,8,57,57,0,0,0-98.16,0,8,8,0,1,1-13.84-8,72.06,72.06,0,0,1,33.74-29.92,48,48,0,1,1,58.36,0A72.06,72.06,0,0,1,190.92,212ZM128,176a32,32,0,1,0-32-32A32,32,0,0,0,128,176ZM72,120a8,8,0,0,0-8-8A24,24,0,1,1,87.24,82a8,8,0,1,0,15.5-4A40,40,0,1,0,37,117.51,67.94,67.94,0,0,0,9.6,139.19a8,8,0,1,0,12.8,9.61A51.6,51.6,0,0,1,64,128,8,8,0,0,0,72,120Z"></path>
-        </svg>
-      ),
-      title: "Join or create a game round",
-      description: "You can join a game round that someone else has created, or you can create your own. If you create your own, you'll need to choose a prize and set an entry fee."
-    },
-    {
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
-          <path d="M232,64H208V56a16,16,0,0,0-16-16H64A16,16,0,0,0,48,56v8H24A16,16,0,0,0,8,80V96a40,40,0,0,0,40,40h3.65A80.13,80.13,0,0,0,120,191.61V216H96a8,8,0,0,0,0,16h64a8,8,0,0,0,0-16H136V191.58c31.94-3.23,58.44-25.64,68.08-55.58H208a40,40,0,0,0,40-40V80A16,16,0,0,0,232,64ZM48,120A24,24,0,0,1,24,96V80H48v32q0,4,.39,8Zm144-8.9c0,35.52-28.49,64.64-63.51,64.9H128a64,64,0,0,1-64-64V56H192ZM232,96a24,24,0,0,1-24,24h-.5a81.81,81.81,0,0,0,.5-8.9V80h24Z"></path>
-        </svg>
-      ),
-      title: "Compete with other players to win",
-      description: "In a game round, you'll compete with other players to earn points. The player with the most points at the end of the game round wins the prize."
-    },
-    {
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
-          <path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm64-88a8,8,0,0,1-8,8H128a8,8,0,0,1-8-8V72a8,8,0,0,1,16,0v48h48A8,8,0,0,1,192,128Z"></path>
-        </svg>
-      ),
-      title: "Complete in-game challenges to earn points",
-      description: "In a game round, you'll compete with other players to earn points. The player with the most points at the end of the game round wins the prize."
-    },
-    {
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
-          <path d="M216,72H180.92c.39-.33.79-.65,1.17-1A29.53,29.53,0,0,0,192,49.57,32.62,32.62,0,0,0,158.44,16,29.53,29.53,0,0,0,137,25.91a54.94,54.94,0,0,0-9,14.48,54.94,54.94,0,0,0-9-14.48A29.53,29.53,0,0,0,97.56,16,32.62,32.62,0,0,0,64,49.57,29.53,29.53,0,0,0,73.91,71c.38.33.78.65,1.17,1H40A16,16,0,0,0,24,88v32a16,16,0,0,0,16,16v64a16,16,0,0,0,16,16H200a16,16,0,0,0,16-16V136a16,16,0,0,0,16-16V88A16,16,0,0,0,216,72ZM149,36.51a13.69,13.69,0,0,1,10-4.5h.49A16.62,16.62,0,0,1,176,49.08a13.69,13.69,0,0,1-4.5,10c-9.49,8.4-25.24,11.36-35,12.4C137.7,60.89,141,45.5,149,36.51Zm-64.09.36A16.63,16.63,0,0,1,96.59,32h.49a13.69,13.69,0,0,1,10,4.5c8.39,9.48,11.35,25.2,12.39,34.92-9.72-1-25.44-4-34.92-12.39a13.69,13.69,0,0,1-4.5-10A16.6,16.6,0,0,1,84.87,36.87ZM40,88h80v32H40Zm16,48h64v64H56Zm144,64H136V136h64Zm16-80H136V88h80v32Z"></path>
-        </svg>
-      ),
-      title: "Climb the leaderboard for a chance to win",
-      description: "In a game round, you'll compete with other players to earn points. The player with the most points at the end of the game round wins the prize."
-    },
-    {
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
-          <path d="M128,88a40,40,0,1,0,40,40A40,40,0,0,0,128,88Zm0,64a24,24,0,1,1,24-24A24,24,0,0,1,128,152ZM240,56H16a8,8,0,0,0-8,8V192a8,8,0,0,0,8,8H240a8,8,0,0,0,8-8V64A8,8,0,0,0,240,56ZM193.65,184H62.35A56.78,56.78,0,0,0,24,145.65v-35.3A56.78,56.78,0,0,0,62.35,72h131.3A56.78,56.78,0,0,0,232,110.35v35.3A56.78,56.78,0,0,0,193.65,184ZM232,93.37A40.81,40.81,0,0,1,210.63,72H232ZM45.37,72A40.81,40.81,0,0,1,24,93.37V72ZM24,162.63A40.81,40.81,0,0,1,45.37,184H24ZM210.63,184A40.81,40.81,0,0,1,232,162.63V184Z"></path>
-        </svg>
-      ),
-      title: "Use your points to buy powerups",
-      description: "In a game round, you'll compete with other players to earn points. The player with the most points at the end of the game round wins the prize."
-    },
-    {
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
-          <path d="M128,88a40,40,0,1,0,40,40A40,40,0,0,0,128,88Zm0,64a24,24,0,1,1,24-24A24,24,0,0,1,128,152ZM240,56H16a8,8,0,0,0-8,8V192a8,8,0,0,0,8,8H240a8,8,0,0,0,8-8V64A8,8,0,0,0,240,56ZM193.65,184H62.35A56.78,56.78,0,0,0,24,145.65v-35.3A56.78,56.78,0,0,0,62.35,72h131.3A56.78,56.78,0,0,0,232,110.35v35.3A56.78,56.78,0,0,0,193.65,184ZM232,93.37A40.81,40.81,0,0,1,210.63,72H232ZM45.37,72A40.81,40.81,0,0,1,24,93.37V72ZM24,162.63A40.81,40.81,0,0,1,45.37,184H24ZM210.63,184A40.81,40.81,0,0,1,232,162.63V184Z"></path>
-        </svg>
-      ),
-      title: "Get ahead by buying more points",
-      description: "In a game round, you'll compete with other players to earn points. The player with the most points at the end of the game round wins the prize."
-    }
-  ];
 
-  const games = [
-    {
-      image: "https://cdn.usegalileo.ai/sdxl10/fe7e2cf2-6b5e-411e-93ee-0c9efe332a4d.png",
-      title: "Game of Thrones",
-      time: "Starts in 2 hours"
-    },
-    {
-      image: "https://cdn.usegalileo.ai/sdxl10/540543c0-09d5-4e55-9dd2-2c1b29f2b982.png",
-      title: "Fantasy Football",
-      time: "Starts in 4 hours"
-    },
-    {
-      image: "https://cdn.usegalileo.ai/sdxl10/37a376a9-ec2f-4409-bcd1-c2f1063d89fa.png",
-      title: "Academy Awards",
-      time: "Starts in 6 hours"
-    }
-  ];
+export default function Home() {
+  const [count, setCount] = useState(0)
+  const { t, i18n } = useTranslation()
+  const { isDarkMode, toggleTheme } = useTheme()
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng)
+  }
+
+  // 현재 설정된 언어 코드 반환
+  const getCurrentLanguage = () => {
+    return i18n.language
+  }
+
+  // 현재 언어가 지정된 언어와 일치하는지 확인
+  const isCurrentLanguage = (lng) => {
+    return getCurrentLanguage().startsWith(lng)
+  }
 
   return (
-    <div className="design-root">
-      <div className="layout-container">
-        <header className="header">
-          <div className="flex items-center gap-4 text-white">
-            <div className="header-logo">
-              <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M6 6H42L36 24L42 42H6L12 24L6 6Z" fill="currentColor"></path>
-              </svg>
-            </div>
-            <h2 className="text-white text-lg font-bold leading-tight tracking-[-0.015em]">King Game</h2>
-          </div>
-          <div className="flex flex-1 justify-end gap-8">
-            <div
-              className="header-avatar"
-              style={{ backgroundImage: 'url("https://cdn.usegalileo.ai/sdxl10/280a1a13-b902-4048-8b49-8256367f91eb.png")' }}
-            ></div>
-          </div>
-        </header>
-
-        <div className="px-40 flex flex-1 justify-center py-5">
-          <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
-            <div className="@container">
-              <div className="@[480px]:p-4">
-                <div
-                  className="hero-section"
-                  style={{ backgroundImage: 'url("https://cdn.usegalileo.ai/sdxl10/03f84dc7-ad7e-4def-889a-99b790311101.png")' }}
-                >
-                  <div className="hero-content">
-                    <h1 className="hero-title">Play for the throne</h1>
-                    <h2 className="hero-subtitle">Join a game round and compete with other players to win big</h2>
-                  </div>
-                  <button className="primary-button">
-                    <span className="truncate">Join now</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-
-            <h2 className="section-title">How it works</h2>
-            <div className="grid grid-cols-[repeat(auto-fit,minmax(158px,1fr))] gap-3 p-4">
-              {features.map((feature, index) => (
-                <div key={index} className="feature-card">
-                  <div className="feature-icon">{feature.icon}</div>
-                  <div className="flex flex-col gap-1">
-                    <h2 className="feature-title">{feature.title}</h2>
-                    <p className="feature-description">{feature.description}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-
-            <h2 className="section-title">Available game rounds</h2>
-            {games.map((game, index) => (
-              <div key={index} className="game-card">
-                <div className="flex items-center gap-4">
-                  <div
-                    className="game-image"
-                    style={{ backgroundImage: `url("${game.image}")` }}
-                  ></div>
-                  <div className="flex flex-col justify-center">
-                    <p className="game-title">{game.title}</p>
-                    <p className="game-time">{game.time}</p>
-                  </div>
-                </div>
-                <div className="shrink-0">
-                  <button className="secondary-button">
-                    <span className="truncate">Join</span>
-                  </button>
-                </div>
-              </div>
-            ))}
-          </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>WANG {import.meta.env.VITE_APP_TITLE} {import.meta.env.VITE_BASE_URL}</h1>
+      <div className="language-selector">
+        <p>{t('language')}:</p>
+        <button
+          onClick={() => changeLanguage('ko')}
+          className={isCurrentLanguage('ko') ? 'active' : ''}
+        >
+          한국어
+        </button>
+        <button
+          onClick={() => changeLanguage('en')}
+          className={isCurrentLanguage('en') ? 'active' : ''}
+        >
+          English
+        </button>
+        <div className="current-language">
+          {t('currentLanguage')}: {getCurrentLanguage()}
         </div>
       </div>
-    </div>
-  );
-};
 
-export default Home; 
+      <div className="theme-toggle">
+        <button
+          onClick={toggleTheme}
+          className="theme-button"
+        >
+          {isDarkMode ? t('lightMode') : t('darkMode')}
+          <span className="theme-icon">{isDarkMode ? '☀️' : '🌙'}</span>
+        </button>
+      </div>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          {t('counter', { count })}
+        </button>
+        <p>
+          {t('editText')}
+        </p>
+      </div>
+      <p className="read-the-docs">
+        {t('readDocs')}
+      </p>
+    </>
+  )
+}
