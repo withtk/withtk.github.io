0945a32e4c9a63dccab09cc9f61379aa1a1bb7ee
mcPro <dnf126@gmail.com>
2025-06-09 07:21:57 +0900

    ls | add index.html

--- lscode/src/List.vue
+++ lscode/src/List.vue
@@ -8,29 +8,29 @@
 </template>
 
 <script setup lang="ts">
-import { type Ref, ref } from 'vue';
-import page_list from '../node/page_list.json';
-import {exceptionList,arrToEmphasize} from './util/constants.ts'
+import { type Ref, ref } from 'vue'
+import page_list from '../node/page_list.json'
+import { exceptionList, arrToEmphasize } from './util/constants.ts'
 
 // const exceptionList = ['ls', 'lscode', 'blogcode', 'milkcode', 'moviecode']; // 제외할 디렉토리
-console.log('--func--page_list: ', page_list);
+// console.log('--func--page_list: ', page_list);
 
-const list: Ref = ref<Array<string>>([]);
-list.value = page_list.filter((element) => !exceptionList.includes(element));
+const list: Ref = ref<Array<string>>([])
+list.value = page_list.filter((element) => !exceptionList.includes(element))
 
-console.log('--func--list.value: ', list.value);
+// console.log('--func--list.value: ', list.value);
 
 const goto = (index: number) => {
-  const url = 'https://withtk.github.io/' + list.value[index];
-  window.open(url, '_blank');
-};
+  const url = 'https://withtk.github.io/' + list.value[index]
+  window.open(url, '_blank')
+}
 
 const getStyleClass = (name: string) => {
   // const arrToEmphasize = ['blog', 'movie', 'milk'];
-  const result = arrToEmphasize.some((str) => name.includes(str));
-  console.log('--getStyleClass--result: ', result);
-  return result ? 'blog' : '';
-};
+  const result = arrToEmphasize.some((str) => name.includes(str))
+  // console.log('--getStyleClass--result: ', result);
+  return result ? 'blog' : ''
+}
 </script>
 
 <style scoped>
