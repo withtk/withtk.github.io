1ecf15983ab64a9b850a8612e15979752f439a49
mcPro <dnf126@gmail.com>
2025-06-08 18:13:34 +0900

    wang | add page to ls

---
+++ lscode/src/util/constants.ts
@@ -0,0 +1,8 @@
+export const Colors = {
+  RED: 'red',
+  GREEN: 'green',
+  BLUE: 'blue',
+} as const
+
+export const exceptionList = ['ls', 'lscode', 'blogcode', 'milkcode', 'moviecode', 'wangcode'] // 제외할 디렉토리
+export const arrToEmphasize = ['blog', 'milk', 'movie', 'wang'] // 버튼 강조할 디렉토리
